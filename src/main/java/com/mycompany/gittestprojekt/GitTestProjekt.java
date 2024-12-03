b9adad0d75dd653c92956f4cbe1302c1f2319bd6
Isatu Sillah <i.sillah@gmx.de>
2024-12-03 09:14:48 +0100

    Projekterstellung

---
+++ src/main/java/com/mycompany/gittestprojekt/GitTestProjekt.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gittestprojekt;
+
+/**
+ *
+ * @author Isatu
+ */
+public class GitTestProjekt {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
